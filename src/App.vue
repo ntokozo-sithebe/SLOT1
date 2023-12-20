dd024b089e26775eec1f48331c91f09494d9ec72
ntokozo-sithebe <ntoky28@gmail.com>
2023-12-20 08:19:05 +0200

    slot

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,39 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Button>
+  <template #click>
+    <div>Hello</div>
+  </template>
+  <template #logo>
+    <div>
+      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-right" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0z"/>
+  <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/>
+</svg>
+     Click 
+    </div>
+  </template>
+</Button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Button from './components/Button.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Button
+  },
+  data(){
+    return{
+      display: 'Ntokozo',
+      image: ''
+    }
+
+},
+
 }
+  
+
 </script>
 
 <style>
@@ -23,4 +45,6 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
+
 </style>
---
+++ src/components/Button.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <button class="mybtn"> 
+            <slot name="click"></slot>
+        </button>
+        <div>
+            <button>
+                <slot name="logo"> </slot>
+            </button>
+        </div>
+        
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'ButtonComp'
+    }
+</script>
+
+<style scoped>
+
+</style>
